c2939d5bfdb69dfb63598632fc7754f7c76aa3bd
sonogodo <adrexpert@gmail.com>
2024-09-26 23:25:39 -0300

    900

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ let numeroLimite = 9;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
-}
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 101');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 900');
 }
 
 exibirMensagemInicial();
